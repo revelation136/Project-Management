930038e16035fa499dfe9bae310a4b962d5fb960
revelation136 <revelation136@gmail.com>
2023-02-03 21:31:45 +0800

    adding models to models.py, adding values to columns by using shell

--- project_management/project_management/settings.py
+++ project_management/project_management/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     # my apps
     'home',
+    'work_force',
 
 
     'django.contrib.admin',
--- project_management/work_force/models.py
+++ project_management/work_force/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+
 # Create your models here.
+
+
+class Manpower(models.Model):
+    first = models.CharField(max_length=64, default="")
+    last = models.CharField(max_length=64, default="")
+    rate = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.id}: {self.last} {self.first} {self.rate}"
