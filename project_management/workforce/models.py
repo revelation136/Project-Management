857a7707e9b615f843d36bf84051f50babbb7919
revelation136 <revelation136@gmail.com>
2023-02-21 13:51:53 +0800

    adding more column to models.Manpower

--- project_management/workforce/models.py
+++ project_management/workforce/models.py
@@ -7,7 +7,8 @@ from django.db import models
 class Manpower(models.Model):
     first = models.CharField(max_length=64, default="")
     last = models.CharField(max_length=64, default="")
+    position = models.CharField(max_length=64, default="")
     rate = models.IntegerField()
 
     def __str__(self):
-        return f"{self.id}: {self.last} {self.first} {self.rate}"
+        return f"{self.id}: {self.last}, {self.first} | {self.position} ({self.rate})"
